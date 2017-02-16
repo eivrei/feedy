5a3e744ba1cf691b9156c3644bc4f3347d0ecdae
Eivind Klovjan <eivind.klo@gmail.com>
2017-02-16 15:28:15 +0100

    added zip extraction to pptx_to_xml

--- server/pptx_to_xml.py
+++ server/pptx_to_xml.py
@@ -1,4 +1,7 @@
-import zipfile
+# Note: susceptible to zip bombs
+
+
+from zipfile import ZipFile
 import os
 from shutil import copyfile
 
@@ -7,31 +10,32 @@ class PptxToXml:
     # INPUTS:
     # pptx_loc - location of pptx file to be converted
     # save_dir - directory in which converted zip file is to be extracted
-    def __init__(self, pptx_loc, save_dir=''):
-        self.pptx_dir = pptx_loc  # 'self.pptx_dir' contains the directory location of the pptx file
-        self.save_dir = save_dir
-        self.zip_dir = None
+    def __init__(self, pptx_name):
+        self.pptx_name = pptx_name  # self.pptx_dir - name of the pptx file
+        self.zip_name = None
 
     # Creates a new .zip file based on .pptx
-    def CopyAndRename(self):
-        pos = self.pptx_dir.index('.')
-        pptx_name = self.pptx_dir[:pos]
+    def copy_and_rename(self):
+        pos = self.pptx_name.index('.')
+        pptx_name = self.pptx_name[:pos]
 
-        zip_dir = 'zip_' + pptx_name + '.zip'
-        copyfile(self.pptx_dir, zip_dir)
+        zip_name = 'zip_' + pptx_name + '.zip'  # zip_name - name of converted zip file
+        copyfile('temp\\' + self.pptx_name, 'temp\\' + zip_name)
 
-        self.zip_dir = zip_dir
+        self.zip_name = zip_name
 
     # Extracts zip-file to directory indicated in constructor
-    def ExtractZipFile(self):
-        with zipfile.ZipFile(self.zip_dir, 'r') as zip_file:
-            pass
+    def extract_zip_file(self):
+        with ZipFile('temp\\' + self.zip_name, 'r') as zip_file:
+            extract_dir = 'extraction_folder\\'
+            zip_file.extractall(extract_dir)
 
     # Deletes .pptx and .zip files after they have been converted
-    def DeleteFiles(self):
+    def delete_files(self):
         pass
 
 
 if __name__ == '__main__':
     pptxToXml = PptxToXml('test_1.pptx')
-    pptxToXml.CopyAndRename()
+    pptxToXml.copy_and_rename()
+    pptxToXml.extract_zip_file()
