82e3c4f242200523636dce57506b37652bf2a846
Eivind Reime <eivindreime@dhcp-10-24-18-130.wlan.ntnu.no>
2017-02-16 15:55:58 +0100

    Add data generation to xml_to_raw_data

---
+++ server/xml_to_raw_data.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as etree
+import os.path
+
+
+class XmlToRawData:
+    def __init__(self):
+        self.path = '.extraction_folder/ppt/slides/'  # Set path for extracting of slides
+        self.number_of_slides = len([f for f in os.listdir(self.path) if f.endswith('.xml')
+                                     and os.path.isfile(os.path.join(self.path, f))]) # Find number of slides in folder
+
+    # Generate data from .xml files
+    def generate_data(self):
+        all_data = []
+        for file_nr in range(1, self.number_of_slides + 1):
+            slide_data = []
+            tree = etree.parse(self.path + 'slide' + str(file_nr) + '.xml')
+            for element in tree.iter('{http://schemas.openxmlformats.org/drawingml/2006/main}t'):
+                slide_data.append(element.text)
+            all_data.append(slide_data)
+
+        # To be implemented!
+        # Send data to quiz generator
