55c581cd8c71ac218a518f463e6e0e6054303aab
Eivind Klovjan <eivind.klo@gmail.com>
2017-02-21 13:23:37 +0100

    generalized file locations for pptx_to_xml.py

--- server/pptx_to_xml.py
+++ server/pptx_to_xml.py
@@ -8,26 +8,30 @@ from shutil import copyfile
 
 class PptxToXml:
     # INPUTS:
-    # pptx_name - name of pptx file to be converted
-    def __init__(self, pptx_name):
-        self.pptx_name = pptx_name
-        self.zip_name = None
+    # pptx_dir - directory of pptx file to be converted
+    # zip_folder - folder in which to save zip file
+    # extraction_folder - folder in which to extract zip file
+    def __init__(self, pptx_dir, zip_folder='temp/', extraction_folder='extraction_folder/'):
+        self.pptx_dir = pptx_dir
+        self.zip_folder = zip_folder
+        self.extraction_folder = extraction_folder
+
+        self.dest_zip_dir = None
 
     # Creates a new .zip file based on .pptx
     def copy_and_rename(self):
-        pos = self.pptx_name.index('.')
-        pptx_basename = self.pptx_name[:pos]  # pptx_name - name of pptx file minus extension name
+        pos = self.pptx_dir('.')
+        pptx_base_dir = self.pptx_dir[:pos]  # pptx_base_dir - directory of pptx file minus file extension '.pptx'
 
-        zip_name = 'zip_' + pptx_basename + '.zip'  # zip_name - name of converted zip file
-        copyfile('temp\\' + self.pptx_name, 'temp\\' + zip_name)
+        zip_dir = pptx_base_dir + '_zip.zip'  # zip_dir - directory of converted zip file
+        copyfile(self.pptx_dir, zip_dir)
 
-        self.zip_name = zip_name
+        self.dest_zip_dir = self.zip_folder + zip_dir
 
     # Extracts zip-file to directory indicated in constructor
     def extract_zip_file(self):
-        with ZipFile('temp\\' + self.zip_name, 'r') as zip_file:
-            extract_dir = 'extraction_folder\\'
-            zip_file.extractall(extract_dir)
+        with ZipFile(self.dest_zip_dir, 'r') as zip_file:
+            zip_file.extractall(self.extraction_folder)
 
     # Deletes .pptx and .zip files after they have been converted
     def delete_files(self):
