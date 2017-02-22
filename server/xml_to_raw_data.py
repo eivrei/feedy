0c105ed2356007482101707f27ad8c6bc9f7859e
Eivind Reime <eivindreime@Eivinds-MacBook-Pro.local>
2017-02-22 21:37:28 +0100

    Update to gitignore

--- server/xml_to_raw_data.py
+++ server/xml_to_raw_data.py
@@ -4,7 +4,8 @@ import os.path
 
 class XmlToRawData:
     def __init__(self):
-        self.path = '.extraction_folder/ppt/slides/'  # Set path for extracting of slides
+        self.path = "./ppt_template/basic_ppt/ppt/slides/"
+        # self.path = './extraction_folder/ppt/slides/'  # Set path for extracting of slides
         self.number_of_slides = len([f for f in os.listdir(self.path) if f.endswith('.xml')
                                      and os.path.isfile(os.path.join(self.path, f))]) # Find number of slides in folder
 
@@ -17,6 +18,12 @@ class XmlToRawData:
             for element in tree.iter('{http://schemas.openxmlformats.org/drawingml/2006/main}t'):
                 slide_data.append(element.text)
             all_data.append(slide_data)
+        return all_data
 
         # To be implemented!
         # Send data to quiz generator
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    xmlToRawData = XmlToRawData()
+    print(xmlToRawData.generate_data())
