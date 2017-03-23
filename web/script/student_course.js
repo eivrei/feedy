a35c15d31fb580127e24a8334c111439ebccbf8c
Alexander Silva <alex.wennevold@gmail.com>
2017-03-23 21:17:55 +0100

    framework for student html, functionality for first page, some code clarifiation for lecturer pages

---
+++ web/script/student_course.js
@@ -0,0 +1,53 @@
+
+
+
+ 
+
+var hash = window.location.hash.substr(1);
+hash = hash.split("_");
+var course_code =  hash[0];
+var parallel_id =  hash[1];
+
+$(document).ready(function() {
+	$.get("../../php/student_getCourse.php?course=" + course_code + "_" + parallel_id, function (data) {
+		console.log(data);
+		if (data != "NO DATA") {
+		$("#course_name").html(course_code);
+		createLectureTable(data); //if on data length?
+		}
+		else {
+		//console.log(data); //why?
+		}
+
+	});	
+});
+/* PSEUDOCODE
+Retrieve anchors
+Give dynamic course name possibly with parallel
+createLectureTable(data)
+
+function CreateLectureTable(data) {
+	
+	while (more lectures) {
+		if (hasname) {
+		create date field
+		create name field
+		add take quiz button
+		}
+		else {
+		create date field
+		add "no quiz available" text
+		}
+	}
+}
+
+function take_quiz(button) {
+	go to take quiz page for correct quiz
+}
+
+function createTakeQuizButtonString(lecture_id) {
+	construct whole button string (not as separate function?)
+	"take quiz"
+	link-string to redirect to correct page
+}
+*/
