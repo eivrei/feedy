af6a4b4c2525ceebee61d2a7a3551cacf0d31392
Eivind Klovjan <eivind.klo@gmail.com>
2017-03-29 12:45:45 +0200

    Merge login feature with develop (#10)
    * added login system (validation, redirect and cookie only)
    
    * updated file structure (old files that need to be in new format in 'old' folder)
    
    * Dynamic retrieval of lecturers courses, with navigation
    
    * reworked lecturer_courses.php to fit with database changes (hopefully, can't test yet)
    
    * changed file structure for css and html files
    
    * retreive date for lecturer/course.html and separete cookie js
    
    * added list/button generator with empty functions on lecturer/course, reworked php work with to new database structure
    
    * dynamic course code in lecturer/course.html, lecture name displays correctly, small code readability improvements
    
    * framework for student html, functionality for first page, some code clarifiation for lecturer pages
    
    * fixed numerous display issues, added complete view quiz funtionality for lecturer. Probably more.
    
    * fixed encoding errors, dynamic names, added statistics functionality

---
+++ web/script/lecturer_course.js
@@ -0,0 +1,83 @@
+var hash = window.location.hash.substr(1);
+hash = hash.split("_");
+var course_code =  hash[0];
+var parallel_id =  hash[1];
+
+$(document).ready(function() {
+	$.get("../../php/lecturer_course.php?course=" + course_code + "_" + parallel_id, function (data) {
+		console.log(data);
+		if (data != "NO DATA") {
+		$("#course_name").html(course_code);
+		createLectureTable(data); //if on data length?
+		}
+		else {
+		//console.log(data); //why?
+		}
+
+	});	
+});
+
+function createButtonString(type, lecture_id, date = "null") { //give date as argument only for type = upload (sysarg), remember to check if id is needed
+	var buttonString = "<button id='" + lecture_id + "' class='btn btn-md btn-primary' type='button'" ; //note: no end >
+	switch(type) {
+		case "view":
+			buttonString += "value=" + lecture_id + " onclick = view_quiz(this)>View quiz";
+			console.log("create view", buttonString);
+			break;
+		case "delete":
+			buttonString += "value=" +lecture_id + " onclick = delete_quiz(this)>Delete quiz";
+			break;
+		case "upload": //create full form instead? Copy from hardcoded lecturer/course.html, choose file will be made here as well
+			buttonString += "value="+ lecture_id + "_" +date+ " onclick = upload_pptx(this)>Upload placeholder"; //see above
+			break;
+		default:
+			console.log("something went wrong");
+	}
+	buttonString +="</button>";
+	console.log(buttonString);
+	return buttonString;
+}
+
+function view_quiz(button) { 
+	var redirectString = "quiz_preview.html#" + button.value;
+	window.location = redirectString;
+}
+
+function delete_quiz(button) {
+	console.log("in delete");
+} 
+
+function upload_pptx(button) { //uses form action for upload.php
+	console.log("in upload");
+}
+
+function createLectureTable(data) { 
+
+	var lectureContent = data.split("|"); //remember to check if pop is needed
+	lectureContent.pop();
+	var toAppend = "<div><span>"; //make divs possibly with id for each
+	
+	for (var i = 0; i < lectureContent.length; i+=3) { //possibly -1 on length comparison
+		var lecture_id = lectureContent[i];
+		var date = lectureContent[i+1];
+		var name = lectureContent[i+2];
+		console.log("name =" ,name);
+		toAppend+= "<a href='statistics.html#" + lecture_id +"'>" + date + "</a>"; //link course name to statistics page
+		if (name != "NULL") { 
+			toAppend += " <i>" + name + "</i></span>";  
+			toAppend += " " + createButtonString("view",lecture_id); //create view button value = lecture_id
+			toAppend += " " + createButtonString("delete",lecture_id); //create delete button value = lecture_id
+		}
+		else {
+			toAppend+= " " + createButtonString("upload", lecture_id, date); //only make var date if this case applies?
+			//create upload button value = lecture_id + lecture.date (?)
+		}
+		toAppend+="</div>";
+	}
+		//toAppend += "</br>";
+		console.log(toAppend);
+		$("#course_content").append(toAppend);
+}
+
+
+
