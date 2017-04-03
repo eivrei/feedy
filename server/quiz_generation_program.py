05c31992a33486d5db5bce68991b4d1e874f0456
Eivind Reime <zweebe@users.noreply.github.com>
2017-04-03 21:10:43 +0200

    Add functionality so quiz_generation_program deletes .pptx file after uploading quiz to database.

--- server/quiz_generation_program.py
+++ server/quiz_generation_program.py
@@ -1,12 +1,16 @@
 import sys
-from server.pptx_extraction import extract
-from server.quiz_generator import *
-from server.quiz_sender import QuizSender
+import os
+from pptx_extraction import extract
+from quiz_generator import *
+from quiz_sender import QuizSender
 
 
 def main(filename, lecture_id):
+    # Set path to .pptx file
+    path = "/home/groupswww/pugruppe100/pugruppe100/server/temp/" + filename + ".pptx"
+
     # Generate raw data from pptx file
-    raw_data = extract("/home/groupswww/pugruppe100/pugruppe100/server/temp/" + filename + ".pptx")
+    raw_data = extract(path)
 
     # Generate quiz data from raw_data
     quiz_generator = QuizGenerator(raw_data)
@@ -15,6 +19,9 @@ def main(filename, lecture_id):
     # Send quiz to db
     quiz_sender = QuizSender(lecture_id, quiz_generator.quiz)
     quiz_sender.run()
+
+    # Remove .pptx file from temp folder after quiz is generated
+    os.remove(path)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
