bbe2b7dacf2e908c2d3591a33c993de6ca6e1b3d
zweebe <zweebe@users.noreply.github.com>
2017-04-26 20:06:39 +0200

    Fix generate statistics error

--- web/php/lecturer_generateStatistics.php
+++ web/php/lecturer_generateStatistics.php
@@ -1,5 +1,5 @@
 <?php
-    $lecture_id = $_GET["lecture_id"];
+    $lecture_id = $_GET["lecture"];
     $program_dir = escapeshellarg("../../server/statistics.py");
     echo exec("../../../python_env/bin/python3 $program_dir $lecture_id");
     echo "OK";
